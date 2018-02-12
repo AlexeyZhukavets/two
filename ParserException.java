f21007beac889b6c488babe7eba8165cbdf3a1fe
Alex <alexeyzhukavets@tut.by>
2018-02-12 17:44:20 +0300

    first commit

---
+++ ParserException.java
@@ -0,0 +1,18 @@
+package acmp.parseexpression;
+
+/**
+ * Created by alexe_000 on 11.02.2018.
+ */
+public class ParserException extends Exception{
+
+    private String errStr;  //  Описание ошибки
+
+    public ParserException(String errStr) {
+        super();
+        this.errStr = errStr;
+    }
+
+    public String toString(){
+        return this.errStr;
+    }
+}
